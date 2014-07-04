5318d662c9e7fd941e32463bda12ed5fc47c7d8f
begemotv2718 <begemotv2718@gmail.com>
2014-07-04 20:38:01 +0300

    Working modification

--- display.h
+++ display.h
@@ -1,3 +1,3 @@
 void init_display(uint8_t contrast);
-void putsxy(uint8_t x, uint8_t y, char *msg);
+void putsxy(uint8_t x, uint8_t y, unsigned char *msg);
 void upload_box(uint8_t x, uint8_t y, uint8_t dx, uint8_t dy);
--- main.c
+++ main.c
@@ -1,26 +1,154 @@
-#define F_CPU 12000000UL                                    /* Clock Frequency = 1Mhz */
-#include <avr/io.h>
-#include <util/delay.h>
-#include <inttypes.h>
-#include "display.h"
     
-int main(void){                         // The main function
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>  /* for sei() */
+#include <util/delay.h>     /* for _delay_ms() */
+#include <avr/eeprom.h>
 
-  DDRD=0b11111111;
-  DDRC=0b00111110;
-  DDRB=0b00000001;
-  PORTB=1;
+#include <avr/pgmspace.h>   /* required by usbdrv.h */
+#include "usbdrv.h"
+#include "oddebug.h"        /* This is also an example for using debug macros */
+#include "display.h"
+#include "string.h"
 
+/* ------------------------------------------------------------------------- */
+/* ----------------------------- USB interface ----------------------------- */
+/* ------------------------------------------------------------------------- */
 
+PROGMEM const char usbHidReportDescriptor[22] = {    /* USB report descriptor */
+    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
+    0x09, 0x01,                    // USAGE (Vendor Usage 1)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
+    0x75, 0x08,                    //   REPORT_SIZE (8)
+    0x95, 0x80,                    //   REPORT_COUNT (128)
+    0x09, 0x00,                    //   USAGE (Undefined)
+    0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
+    0xc0                           // END_COLLECTION
+};
+/* Since we define only one feature report, we don't use report-IDs (which
+ * would be the first byte of the report). The entire report consists of 128
+ * opaque data bytes.
+ */
 
+/* The following variables store the status of the current data transfer */
+static uchar    currentAddress;
+static uchar    bytesRemaining;
+static uchar    buffer[128];
 
-  init_display(15);
-  while (1) {                        // Set up an infinite loop
-
-  putsxy(0,0, "987");
-  putsxy(5,2, "12:34");  
-  _delay_ms(3000);
-
+void do_display(void){
+  buffer[18]=0;
+  putsxy(buffer[0],buffer[1],&buffer[2]);
+  /*
+  for(uchar y=0;y<6;y++){
+     strncpy(buf2,&buffer[16*y],16);
+     buf2[16]=0;
+     putsxy(buf2,0,y);
   }
-
+  */
 }
+/* ------------------------------------------------------------------------- */
+
+/* usbFunctionRead() is called when the host requests a chunk of data from
+ * the device. For more information see the documentation in usbdrv/usbdrv.h.
+ */
+uchar   usbFunctionRead(uchar *data, uchar len)
+{
+    if(len > bytesRemaining)
+        len = bytesRemaining;
+    for(uchar i=0; i<len;i++){
+      data[i]=buffer[currentAddress+i];
+    }
+    currentAddress += len;
+    bytesRemaining -= len;
+    return len;
+}
+
+/* usbFunctionWrite() is called when the host sends a chunk of data to the
+ * device. For more information see the documentation in usbdrv/usbdrv.h.
+ */
+uchar   usbFunctionWrite(uchar *data, uchar len)
+{
+    if(bytesRemaining == 0){
+        do_display();
+        return 1;               /* end of transfer */
+    }
+    if(len > bytesRemaining)
+        len = bytesRemaining;
+    for(uchar i=0;i<len;i++){
+      buffer[currentAddress+i]=data[i];
+    }
+    currentAddress += len;
+    bytesRemaining -= len;
+    if(bytesRemaining == 0){
+        do_display();
+        return 1;               /* end of transfer */
+    }else{
+      return 0;
+    }
+    
+}
+
+
+
+/* ------------------------------------------------------------------------- */
+
+usbMsgLen_t usbFunctionSetup(uchar data[8])
+{
+usbRequest_t    *rq = (void *)data;
+
+    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* HID class request */
+        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+            /* since we have only one report type, we can ignore the report-ID */
+            bytesRemaining = 20;
+            currentAddress = 0;
+            return USB_NO_MSG;  /* use usbFunctionRead() to obtain data */
+        }else if(rq->bRequest == USBRQ_HID_SET_REPORT){
+            /* since we have only one report type, we can ignore the report-ID */
+            bytesRemaining = 18;
+            currentAddress = 0;
+            return USB_NO_MSG;  /* use usbFunctionWrite() to receive data from host */
+        }
+    }else{
+        /* ignore vendor type requests, we don't use any */
+    }
+    return 0;
+}
+
+/* ------------------------------------------------------------------------- */
+
+int main(void)
+{
+uchar   i;
+
+    wdt_enable(WDTO_1S);
+    /* Even if you don't use the watchdog, turn it off here. On newer devices,
+     * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
+     */
+    /* RESET status: all port bits are inputs without pull-up.
+     * That's the way we need D+ and D-. Therefore we don't need any
+     * additional hardware initialization.
+     */
+    DDRC=0b00111110;
+    init_display(15);
+    odDebugInit();
+    DBG1(0x00, 0, 0);       /* debug output: main starts */
+    usbInit();
+    usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
+    i = 0;
+    while(--i){             /* fake USB disconnect for > 250 ms */
+        wdt_reset();
+        _delay_ms(1);
+    }
+    usbDeviceConnect();
+    sei();
+    DBG1(0x01, 0, 0);       /* debug output: main loop starts */
+    for(;;){                /* main event loop */
+        DBG1(0x02, 0, 0);   /* debug output: main loop iterates */
+        wdt_reset();
+        usbPoll();
+    }
+    return 0;
+}
+
